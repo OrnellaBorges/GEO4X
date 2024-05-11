d598d992cd6601c94bde0fe3ac8b17a1acd24bc9
Ornella B <borges.ornella@gmail.com>
2024-05-11 06:28:07 +0200

    install mui icons

--- src/Components/Mui/AccordionV1.tsx
+++ src/Components/Mui/AccordionV1.tsx
@@ -1,9 +1,20 @@
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material";
 
 function AccordionV1() {
-  return <Accordion>Accordion V1</Accordion>;
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1-content"
+        id="panel1-header"
+      >
+        Hello
+      </AccordionSummary>
+    </Accordion>
+  );
 }
 
 export default Accordion;
