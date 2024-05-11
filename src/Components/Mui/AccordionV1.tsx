4581c539da3a8e2072044ecdc294e9ab99df82dd
Ornella B <borges.ornella@gmail.com>
2024-05-11 06:39:38 +0200

    first basic accordion

--- src/Components/Mui/AccordionV1.tsx
+++ src/Components/Mui/AccordionV1.tsx
@@ -1,9 +1,9 @@
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
-import ExpandMoreIcon from "@mui/icons-material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-function AccordionV1() {
+export default function AccordionV1() {
   return (
     <Accordion>
       <AccordionSummary
@@ -11,10 +11,8 @@ function AccordionV1() {
         aria-controls="panel1-content"
         id="panel1-header"
       >
-        Hello
+        Accordion #1
       </AccordionSummary>
     </Accordion>
   );
 }
-
-export default Accordion;
