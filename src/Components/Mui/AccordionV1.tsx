428dfef1274283baa800ff9236a36b7cc8245f1a
Ornella B <borges.ornella@gmail.com>
2024-05-12 11:15:15 +0200

    save

--- src/Components/Mui/AccordionV1.tsx
+++ src/Components/Mui/AccordionV1.tsx
@@ -5,14 +5,25 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function AccordionV1() {
   return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1-content"
-        id="panel1-header"
-      >
-        Accordion #1
-      </AccordionSummary>
-    </Accordion>
+    <>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Accordion Title 1
+        </AccordionSummary>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Accordion #2
+        </AccordionSummary>
+      </Accordion>
+    </>
   );
 }
