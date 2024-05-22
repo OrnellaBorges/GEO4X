d8304ab57bf90b8bb27802f2c2e4ecc7b4fb55c4
Ornella B <borges.ornella@gmail.com>
2024-05-23 00:46:27 +0200

    need to sleep and continue tomorrow to improve

--- src/Components/Mui/Accordions/Accordions.tsx
+++ src/Components/Mui/Accordions/Accordions.tsx
@@ -8,18 +8,20 @@ import {
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import { accordionData } from "../../../api/accordionData";
+import { count } from "console";
 
 export default function Accordions() {
   //States
   const [expandedPanels, setExpandedPanels] = useState<number[]>([]); // Etat de stockage des index ouverts
   const [availableHeight, setAvailableHeight] = useState<number>(0);
   const [detailsHeights, setDetailsHeights] = useState<number[]>([]);
-
+  const [accordHeight, setAccordHeight] = useState<number[]>([]);
+  const [countBigPanels, setCountBigPanels] = useState<number>(0);
   //refs
   const accordContainerRef = useRef<HTMLDivElement | null>(null);
   const accordsRefs = useRef<(HTMLElement | null)[]>([]);
   const detailsRefs = useRef<(HTMLDivElement | null)[]>([]);
-  console.log("detailsRefs", detailsRefs);
+  //console.log("detailsRefs", detailsRefs);
 
   //petit fonction pour colorer temporairement les accordions
   const getBackground = (index: number) => {
@@ -57,8 +59,35 @@ export default function Accordions() {
 
   //fonction pour updater la hauteur dispo  =>>> a utiliser dans le handleChange
 
-  const updateAvailableHeight = () => {
+  const updateAvailableHeight = (panelIndex: number, newExpanded: boolean) => {
     console.log("j'ajuste la hauteur dispo");
+    //recup hauteur d'un panel
+    const detailHeight = detailsHeights[panelIndex];
+    console.log("hauteur d'un detail ouvert", detailHeight);
+    //si hauteur du detail plus petit que espace dispo
+    if (detailHeight < availableHeight) {
+      // si newExpanded === true
+      if (newExpanded) {
+        setAvailableHeight((prevValue) => prevValue - detailHeight);
+      } else {
+        setAvailableHeight((prevValue) => prevValue + detailHeight);
+      }
+    }
+  };
+
+  //fonction pour determiner la Hauteur maximal d'expansion d'un panel
+  //en fonction de detail height
+
+  const limitExpanded = (accordIndex: number, newExpanded: boolean) => {
+    console.log("panel selected = ", accordIndex);
+
+    console.log("determiner la limite");
+    const detailHeight = detailsHeights[accordIndex];
+    console.log("detailHeight", detailHeight);
+    if (detailHeight > availableHeight) {
+      if (newExpanded) setCountBigPanels((prevCount) => prevCount + 1);
+      else setCountBigPanels((prevCount) => prevCount - 1);
+    }
   };
 
   const handleChange = (panelIndex: number, newExpanded: boolean) => {
@@ -66,10 +95,11 @@ export default function Accordions() {
 
     //au changement je dois mettre a jour ce state pour connaitre lesquels sont ouvert
     updateExpandedPanels(panelIndex);
+    // recalculer la hauteur dispo
+    updateAvailableHeight(panelIndex, newExpanded);
 
-    // recuperer la hauteur des accordions ouverts
-
-    // updateAvailableHeight();
+    //determiner une limite
+    limitExpanded(panelIndex, newExpanded);
   };
 
   useEffect(() => {
@@ -83,14 +113,16 @@ export default function Accordions() {
       (ref) => ref?.offsetHeight || 0
     );
     console.log("accordHeights", accordsHeights);
+    setAccordHeight(accordsHeights);
+
     const sumOfAccordsHeightsClosed = accordsHeights.reduce(
       (acc, currentHeight) => acc + currentHeight,
       0
     );
-    console.log(
+    /* console.log(
       "hauteur dispo =",
       maxLimitContainer - sumOfAccordsHeightsClosed
-    );
+    ); */
 
     // mise a jour de la place dispo
     setAvailableHeight(window.innerHeight - sumOfAccordsHeightsClosed);
@@ -102,6 +134,10 @@ export default function Accordions() {
     console.log("heightDetails", heightDetails);
     setDetailsHeights(heightDetails);
   }, []);
+
+  console.log("availableHeight", availableHeight);
+
+  console.log("countBigPannels", countBigPanels);
 
   return (
     <div
@@ -130,7 +166,12 @@ export default function Accordions() {
             ref={(el) =>
               (detailsRefs.current[accordIndex] = el as HTMLDivElement)
             }
-            sx={{ maxHeight: "200px", overflow: "scroll" }}
+            sx={{
+              maxHeight: countBigPanels
+                ? availableHeight / countBigPanels
+                : "auto",
+              overflow: "scroll",
+            }}
           >
             {accordion.content}
           </AccordionDetails>
