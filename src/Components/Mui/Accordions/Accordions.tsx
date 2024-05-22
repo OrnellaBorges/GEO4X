e1acc128f5c6d5637fbb22529ab4436f27e8e305
Ornella B <borges.ornella@gmail.com>
2024-05-22 11:04:23 +0200

    move key prop

--- src/Components/Mui/Accordions/Accordions.tsx
+++ src/Components/Mui/Accordions/Accordions.tsx
@@ -8,13 +8,12 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import { accordionData } from "../../../Api/accordionData";
 
-export default function AccordionV1() {
+export default function Accordions() {
   return (
     <>
       {accordionData.map((accordion, accordIndex) => (
-        <Accordion>
+        <Accordion key={`${accordion.summary}-${accordIndex}`}>
           <AccordionSummary
-            key={`${accordion.summary}-${accordIndex}`}
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1-content"
             id="panel1-header"
