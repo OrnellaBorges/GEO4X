868cb93e1d9f604d67df1a0edf61f75fde62ffaa
Ornella B <borges.ornella@gmail.com>
2024-05-22 22:53:46 +0200

    need contents height of details to adapt the maxHeight

--- src/Components/Mui/Accordions/Accordions.tsx
+++ src/Components/Mui/Accordions/Accordions.tsx
@@ -12,18 +12,14 @@ import { accordionData } from "../../../api/accordionData";
 export default function Accordions() {
   //States
   const [expandedPanels, setExpandedPanels] = useState<number[]>([]); // Etat de stockage des index ouverts
-  console.log("expandedPanels", expandedPanels);
-
   const [availableHeight, setAvailableHeight] = useState<number>(0);
-  //console.log("availableheight", availableHeight);
+  const [detailsHeights, setDetailsHeights] = useState<number[]>([]);
 
+  //refs
   const accordContainerRef = useRef<HTMLDivElement | null>(null);
-  // recup refs des accordions
   const accordsRefs = useRef<(HTMLElement | null)[]>([]);
-  //console.log("accordsRefs", accordsRefs);
-
-  //recup les ref des details = content
   const detailsRefs = useRef<(HTMLDivElement | null)[]>([]);
+  console.log("detailsRefs", detailsRefs);
 
   //petit fonction pour colorer temporairement les accordions
   const getBackground = (index: number) => {
@@ -83,24 +79,14 @@ export default function Accordions() {
     const maxLimitContainer = accordContainerRef.current?.offsetHeight ?? 0; // use nullish operator to avoid undefined or null to typescript
     console.log("maxLimitContainer", maxLimitContainer);
 
-    // une fois que le composant est rendu le composant sont monté le useEffect arrive
-    // mettre a jour tableau accordsRefs => dois avoir une liste de hauteur en nombre de chaque hauteur d'accordion au départ
-
     const accordsHeights = accordsRefs.current.map(
       (ref) => ref?.offsetHeight || 0
     );
     console.log("accordHeights", accordsHeights);
-
-    // faire la somme totale des accordions fermés
-    //utiliser .reduce() prend le tableau des hauteurs
     const sumOfAccordsHeightsClosed = accordsHeights.reduce(
       (acc, currentHeight) => acc + currentHeight,
       0
     );
-    console.log("sumOfAccordsHeightsClosed", sumOfAccordsHeightsClosed);
-
-    //Calculer la hauteur disponible hauteur ecran - somme des hauteurs des accordions fermées
-
     console.log(
       "hauteur dispo =",
       maxLimitContainer - sumOfAccordsHeightsClosed
@@ -108,8 +94,14 @@ export default function Accordions() {
 
     // mise a jour de la place dispo
     setAvailableHeight(window.innerHeight - sumOfAccordsHeightsClosed);
+
+    // recup hauteur des details
+    const heightDetails = detailsRefs.current.map(
+      (ref) => ref?.scrollHeight || 0
+    );
+    setDetailsHeights(heightDetails);
   }, []);
-  //console.log("availableheight", availableHeight);
+
   return (
     <div
       className="accordionContainer"
@@ -137,7 +129,7 @@ export default function Accordions() {
             ref={(el) =>
               (detailsRefs.current[accordIndex] = el as HTMLDivElement)
             }
-            sx={{ maxHeight: "400px", overflow: "scroll" }}
+            sx={{ maxHeight: "200px", overflow: "scroll" }}
           >
             {accordion.content}
           </AccordionDetails>
