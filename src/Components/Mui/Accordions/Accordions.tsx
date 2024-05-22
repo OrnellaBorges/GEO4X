0c580f6ed9784bd14aec65cf3547043ed1438b9d
Ornella B <borges.ornella@gmail.com>
2024-05-22 11:18:24 +0200

    create function to colored each accordion and remove gutters with disablegutters prop

--- src/Components/Mui/Accordions/Accordions.tsx
+++ src/Components/Mui/Accordions/Accordions.tsx
@@ -9,14 +9,30 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { accordionData } from "../../../Api/accordionData";
 
 export default function Accordions() {
+  const getBackground = (index: number) => {
+    switch (index) {
+      case 0:
+        return "lightblue";
+      case 1:
+        return "green";
+      case 2:
+        return "yellow";
+      case 3:
+        return "grey";
+      default:
+        return "white";
+    }
+  };
+
   return (
     <>
       {accordionData.map((accordion, accordIndex) => (
-        <Accordion key={`${accordion.summary}-${accordIndex}`}>
+        <Accordion key={`${accordion.summary}-${accordIndex}`} disableGutters>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1-content"
             id="panel1-header"
+            sx={{ backgroundColor: getBackground(accordIndex) }}
           >
             {accordion.summary}
           </AccordionSummary>
