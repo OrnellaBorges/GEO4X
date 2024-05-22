eb0b3f6c26eaff5b08516ea620c97b88a61066f3
Ornella B <borges.ornella@gmail.com>
2024-05-23 00:58:59 +0200

    save

--- src/api/accordionData.tsx
+++ src/api/accordionData.tsx
@@ -15,8 +15,9 @@ export const accordionData = [
       <Typography>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        veniam, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam,
       </Typography>
     ),
     componentType: "Typography",
